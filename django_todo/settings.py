403a37b535768be4b16778f6522171ebf3e100c7
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-01-10 15:17:56 +0000

    changed debug to False

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7syr&qyfkorf0-c5yv_spguo@o#b#*h^0i-sfj43yyy%$6xq5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://simple-django-todolist.herokuapp.com/']
 
