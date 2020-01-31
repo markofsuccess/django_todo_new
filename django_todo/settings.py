fdd7ffeddc1b1c808de994c296cd9939e545ddd4
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-01-31 14:02:09 +0000

    added import env.py path with if statement if exists

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -18,7 +18,9 @@ if os.environ.get('DEVELOPMENT'):
 else:
     development = False
 
-
+from os import path
+if path.exists("env.py"):
+    import env
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
